968c5ed1505e1643e9ac1eb42377c0b6427e32d9
tanpeng53 <tanpeng53@gmail.com>
2025-03-02 21:39:59 +0800

    Icon component added

--- src/pages/MonthBill/components/DailyBill/index.js
+++ src/pages/MonthBill/components/DailyBill/index.js
@@ -1,6 +1,7 @@
 import classNames from 'classnames'
 import './index.scss'
 import { useMemo , useState} from 'react'
+import Icon from '@/pages/components/icon'
 
 const DailyBill = ({ date, billList }) => {
     const dayResult = useMemo(() => {
@@ -43,6 +44,7 @@ const DailyBill = ({ date, billList }) => {
                 {billList.map(item => {
                     return (
                         <div className="bill" key={item.id}>
+                            <Icon type={item.useFor}/>
                             <div className="detail">
                                 <div className="billType">{item.useFor}</div>
                             </div>
